0abdcdcb61d6b83ad871157aabd7197eedba5ac8
Louis Orleans <louis@orleans.io>
2016-10-24 00:32:59 -0700

    Updating to TS2
    This should fix #1

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,12 @@ let sourcemaps;
 gulp.task('build:typescript', () => {
     if (!typescript) {
         typescript = require('gulp-typescript');
-        tsProject = typescript.createProject('tsconfig.json', {sortOutput: true});
+        tsProject = typescript.createProject('tsconfig.json');
         sourcemaps = require('gulp-sourcemaps');
     }
     let tsResult = tsProject.src()
         .pipe(sourcemaps.init())
-        .pipe(typescript(tsProject));
+        .pipe(tsProject());
 
     return merge([
             tsResult.js
@@ -38,7 +38,7 @@ gulp.task('build:typescript:tests', () => {
     }
     var tsResult = tsProjectTests.src()
         .pipe(sourcemaps.init())
-        .pipe(typescript(tsProjectTests));
+        .pipe(tsProjectTests());
 
     return tsResult.js
             .pipe(sourcemaps.write())
